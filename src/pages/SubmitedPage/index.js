b05a7c47fec711c1fc4ffabd7bc8ee2c43a98e79
omari <omari.jalagania@gmail.com>
2022-03-03 00:27:34 +0400

    get data from server

--- src/pages/SubmitedPage/index.js
+++ src/pages/SubmitedPage/index.js
@@ -1,7 +1,23 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import Accordian from '../../components/Accordian'
+import { useAxios } from '../../hooks/useAxios'
+import env from 'react-dotenv'
 import './SubmitedPage.css'
 const SubmitedPage = () => {
+  const [data, setData] = useState([])
+
+  console.log(data)
+
+  const { response } = useAxios({
+    method: 'get',
+    url: `/applications?token=${env.REACT_APP_TOKEN}`,
+  })
+  useEffect(() => {
+    if (response !== null) {
+      setData(response)
+    }
+  }, [response])
+
   return (
     <div className='submited'>
       <Accordian />
