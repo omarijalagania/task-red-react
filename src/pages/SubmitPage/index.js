884caf3d6db79b6d7b31721caf905d824faa0b48
omari <omari.jalagania@gmail.com>
2022-03-02 18:52:09 +0400

    about validation

--- src/pages/SubmitPage/index.js
+++ src/pages/SubmitPage/index.js
@@ -1,10 +1,14 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import { Link } from 'react-router-dom'
 import Button from '../../components/Button'
+import { DataContext } from '../../Context/dataContext'
+import { useAxios } from '../../hooks/useAxios'
 import '../home/Home.css'
 import './SubmitPage.css'
 
 const SubmitPage = () => {
+  const [personalData, setPersonalData] = useContext(DataContext)
+
   return (
     <div className='submitPage'>
       <Button title='Submit' className='home__button' />
