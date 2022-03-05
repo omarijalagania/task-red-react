f87d7a49a48d91b5c1595849aa7e66fd3d327da9
omari <omari.jalagania@gmail.com>
2022-03-05 15:44:44 +0400

    skill animation

--- src/components/SkillBox/index.js
+++ src/components/SkillBox/index.js
@@ -1,17 +1,25 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { motion } from 'framer-motion'
 import './SkillBox.css'
+
 const SkillBox = ({ skill, onClick }) => {
   return (
-    <div className='skillBox'>
+    <motion.div
+      initial={{ x: `300px`, opacity: 0 }}
+      animate={{ x: 0, opacity: 1 }}
+      transition={{ type: 'spring' }}
+      className='skillBox'
+    >
       <p className='skillBox__language'>{skill.title}</p>
       <p className='skillBox__years'>Years of expirience: {skill.experience}</p>
-      <img
-        onClick={onClick}
-        className='skillBox__delete'
-        src='/images/remove.png'
-        alt='delete'
-      />
-    </div>
+      <div onClick={onClick}>
+        <img
+          className='skillBox__delete'
+          src='/images/remove.png'
+          alt='delete'
+        />
+      </div>
+    </motion.div>
   )
 }
 
