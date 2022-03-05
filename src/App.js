b022803167c444adb861df20a3af131f33b22a46
omari <omari.jalagania@gmail.com>
2022-03-05 10:27:39 +0400

    change home path

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import Home from './Pages/Home'
+import Home from '../src/Pages/Home'
 import { Routes, Route } from 'react-router-dom'
 import Page from './Pages/Personal'
 import SubmitPage from './Pages/SubmitPage'
